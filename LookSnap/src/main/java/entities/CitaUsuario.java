63a2536d7ec06fa6acde95264e454e310d1008ab
Gonzalo Marin <gonzalo.marin01@uceva.edu.co>
2025-04-23 02:02:02 -0500

    se agrego listar y demas componentes del xhtml

---
+++ LookSnap/src/main/java/entities/CitaUsuario.java
@@ -0,0 +1,60 @@
+package entities;
+import java.sql.Date;
+import java.sql.Time;
+
+public class CitaUsuario {
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String direccion;
+    private Date fecha;
+    private Time hora;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+}
