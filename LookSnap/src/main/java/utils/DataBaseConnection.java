d11c46921f4cfb02d73a8307315a13b5b2fcd49e
camiloMena259 <juan.mena01@uceva.edu.co>
2025-05-06 20:31:12 -0500

    se agrego el crud de barberos desde la pestaña de admiistrador

--- LookSnap/src/main/java/utils/DataBaseConnection.java
+++ LookSnap/src/main/java/utils/DataBaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/LookSnapDB";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String user = "postgres";
-    private static final String pass = "gonzalo1220";
+    private static final String pass = "camilomena";
 
 
 
